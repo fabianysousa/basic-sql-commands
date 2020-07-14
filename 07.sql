d19eb021aed1508713f1cfd4261fc4a9c2c157fc
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-07-14 10:43:03 -0300

    Inserindo dados nas tabelas
    Inserindo dados nas tabelas utilizando um dos camando de manipulação de dados (DML) INSERT.

---
+++ 07.sql
@@ -0,0 +1,20 @@
+-- INSERTS
+
+-- Tipos Produtos
+-- INSERT INTO tipos_produtos (descricao) VALUES ('Computadores');
+-- INSERT INTO tipos_produtos (descricao) VALUES ('Impressoras');
+-- INSERT INTO tipos_produtos (descricao) VALUES ('Diversos');
+
+-- Produtos
+-- INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Notebook DELL 1544', 2345.67 , 1);
+-- INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Impr. Jato de Tinta', 456.00 , 2);
+-- INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Mouse sem fio', 45 , 3);
+
+-- Pacientes
+-- INSERT INTO pacientes (nome, endereco, bairro, cidade, estado, cep, data_nascimento)
+-- 		VALUES ('Angelina Jolie', 'Rua da paz, 44', 'Nova Lima', 'Santos', 'SP', '121213213', '1978-04-24');
+
+-- Selects
+-- SELECT * FROM tipos_produtos;
+-- SELECT * FROM produtos;
+-- SELECT * FROM pacientes;

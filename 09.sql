f03c406849e4fb7911a85f8bb68ca8ec8bc9b1fb
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-07-14 10:58:28 -0300

    Alterando e deletando dados das tabelas
    Alterando e deletando dados das tabelas utilizando os camando de manipulação de dados (DML) UPDATE e DELETE.

---
+++ 09.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM tipos_produtos;
+-- SELECT * FROM produtos;
+
+-- UPDATE
+-- UPDATE tipos_produtos SET descricao = 'Nobreak' WHERE id = 2;
+-- UPDATE produtos SET descricao = 'Notebook',  preco = '2800' WHERE id = 2;
+
+-- DELETE
+-- DELETE FROM produtos WHERE id >= 3;
+-- DELETE FROM tipos_produtos WHERE id = 2;
+

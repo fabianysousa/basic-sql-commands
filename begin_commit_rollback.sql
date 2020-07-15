22c6163e464bcb6800d3a2a65401889d1b11ea9a
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-07-15 13:40:12 -0300

    Create begin_commit_rollback.sql

---
+++ begin_commit_rollback.sql
@@ -0,0 +1,15 @@
+-- SELECT * FROM tipos_produtos;
+-- SELECT * FROM produtos;
+
+-- BEGIN TRANSACTION;
+
+-- INSERT INTO tipos_produtos (descricao) VALUES ('Equipamentos');
+-- INSERT INTO tipos_produtos (descricao) VALUES ('Nobreak');
+
+-- COMMIT;
+
+-- BEGIN TRANSACTION;
+-- INSERT INTO tipos_produto (descricao) VALUES ('Tipo A');
+-- INSERT INTO tipos_produto (descricao) VALUES ('Tipo B');
+
+-- ROLLBACK;

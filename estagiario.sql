f911dfa5d63fb33180b8c21111d231e23e716a63
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-07-15 12:02:56 -0300

    Inserido dados na tabela empresa
    Testando tabela com comando de manipulação de dados (DML) UPDATE e DELETE.

---
+++ estagiario.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO empresas (nome, funcionarios, cidade) VALUES ('Apple', 4500, 'Cupertino');
+-- INSERT INTO empresas (nome, funcionarios, cidade) VALUES ('Microsoft', 23500, 'Palo Alto');
+-- INSERT INTO empresas (nome, funcionarios, cidade) VALUES ('Amazon', 9000, 'Santos');
+
+-- SELECT * FROM empresas;
+
+-- UPDATE empresas SET funcionarios = 1500 WHERE id = 3;
+
+-- DELETE FROM empresas WHERE id = 2;
